bd6efffa0593021cb46bcf8c3de317162ea142cd
Alex Wood <alexpwooddev@gmail.com>
2020-07-13 11:09:25 -0500

    add RatingCount ViewComponent to ShoeCard

---
+++ Components/RatingCount.cs
@@ -0,0 +1,30 @@
+﻿using ClimbingShoebox.Models;
+using ClimbingShoebox.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClimbingShoebox.Components
+{
+    public class RatingCount : ViewComponent
+    {
+        private readonly IRatingEntryRepository ratingEntryRepository;
+
+        public RatingCount(IRatingEntryRepository ratingEntryRepository)
+        {
+            this.ratingEntryRepository = ratingEntryRepository;
+        }
+
+        public IViewComponentResult Invoke(int shoeId)
+        {
+            var ratingCountViewModel = new RatingCountViewModel
+            {
+                RatingCount = ratingEntryRepository.AllRatings.Where(r => r.ShoeId == shoeId).Count()
+            };
+
+            return View(ratingCountViewModel);
+        }
+    }
+}
---
+++ ViewModels/RatingCountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClimbingShoebox.ViewModels
+{
+    public class RatingCountViewModel
+    {
+        public int RatingCount { get; set; }
+
+    }
+}
